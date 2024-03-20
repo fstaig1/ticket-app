85162d8827c94e8e335f87d290f30fb1eba86e1c
Finlay Staig <91324517+fstaig1@users.noreply.github.com>
2024-03-20 01:32:39 +0000

    added user auth for login/logout buttons on navbar

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ if __name__ == '__main__':
     app.register_blueprint(venue_blueprint)
     
     with app.app_context():
-        db.create_all()
+        init_db()
     
     app.run(host="127.0.0.1", port="38255", debug=True)
