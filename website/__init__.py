f0e90e14c6ba2b336a6c0c021264da0e8a69322c
Finlay Staig <91324517+fstaig1@users.noreply.github.com>
2024-04-04 15:57:23 +0100

    removed duplicate code

--- website/__init__.py
+++ website/__init__.py
@@ -11,23 +11,9 @@ def app():
     app.config["SECRET_KEY"] = "6LfaX9ocAAAAABWeVBjjXEZTI5tmcxVkO0fDi32J"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ticket.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    """
-    app.config["SQLALCHEMY_ECHO"] = True
-    app.config["SQLALCHEMY_RECORD_QUERIES"] = True
-    """
     db.init_app(app=app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = "users.login"
-    login_manager.init_app(app=app)
-
-    from .models import User
 
     with app.app_context():
         db.create_all()
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
 
     return app
